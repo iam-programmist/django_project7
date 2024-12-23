01213f3460da9b113ef38d1187872e3aebd3194b
alik <qurbonovalijon30@gmail.com>
2024-12-23 18:52:12 +0500

    updated urls based on updated views

--- django_lesson7/settings.py
+++ django_lesson7/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-AUTH_USER_MODEL = 'vacancy.Customuser'
+AUTH_USER_MODEL = 'vacancy.CustomUser'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- vacancy/urls.py
+++ vacancy/urls.py
@@ -2,19 +2,47 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', CategoryListView.as_view(), name='category_list'),
-    path('category/create/', CategoryCreateView.as_view(), name='category_create'),
-    path('category/<int:pk>/update/', CategoryUpdateView.as_view(), name='category_update'),
-    path('category/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
-    path('categories/<int:pk>/vacancies/', VacanciesByCategoryView.as_view(), name='vacancies_by_category'),
-    path('vacancies/', VacancyListView.as_view(), name='vacancy_list'),
-    path('vacancy/<int:pk>/', VacancyDetailView.as_view(), name='vacancy_detail'),
-    path('vacancy/create/', VacancyCreateView.as_view(), name='vacancy_create'),
-    path('vacancy/<int:pk>/update/', VacancyUpdateView.as_view(), name='vacancy_update'),
-    path('vacancy/<int:pk>/delete/', VacancyDeleteView.as_view(), name='vacancy_delete'),
-    path('applications/', ApplicationListView.as_view(), name='application_list'),
-    path('application/<int:pk>/', ApplicationDetailView.as_view(), name='application_detail'),
-    path('application/create/', ApplicationCreateView.as_view(), name='application_create'),
-    path('application/<int:pk>/update/', ApplicationUpdateView.as_view(), name='application_update'),
-    path('application/<int:pk>/delete/', ApplicationDeleteView.as_view(), name='application_delete'),
+    path('countries/', CountryListView.as_view(), name='country-list'),
+    path('countries/<int:pk>/', CountryDetailView.as_view(), name='country-detail'),
+    path('countries/create/', CountryCreateView.as_view(), name='country-create'),
+    path('countries/update/<int:pk>/', CountryUpdateView.as_view(), name='country-update'),
+    path('countries/delete/<int:pk>/', CountryDeleteView.as_view(), name='country-delete'),
+
+    path('cities/', CityListView.as_view(), name='city-list'),
+    path('cities/<int:pk>/', CityDetailView.as_view(), name='city-detail'),
+    path('cities/create/', CityCreateView.as_view(), name='city-create'),
+    path('cities/update/<int:pk>/', CityUpdateView.as_view(), name='city-update'),
+    path('cities/delete/<int:pk>/', CityDeleteView.as_view(), name='city-delete'),
+
+    path('users/', CustomUserListView.as_view(), name='user-list'),
+    path('users/<int:pk>/', CustomUserDetailView.as_view(), name='user-detail'),
+
+    path('profile/<int:pk>/', ProfileDetailView.as_view(), name='profile-detail'),
+    path('profile/update/<int:pk>/', ProfileUpdateView.as_view(), name='profile-update'),
+
+    path('vacancy-categories/', VacancyCategoryListView.as_view(), name='vacancy-category-list'),
+    path('vacancy-categories/<int:pk>/', VacancyCategoryDetailView.as_view(), name='vacancy-category-detail'),
+    path('vacancy-categories/create/', VacancyCategoryCreateView.as_view(), name='vacancy-category-create'),
+    path('vacancy-categories/update/<int:pk>/', VacancyCategoryUpdateView.as_view(), name='vacancy-category-update'),
+    path('vacancy-categories/delete/<int:pk>/', VacancyCategoryDeleteView.as_view(), name='vacancy-category-delete'),
+
+    path('vacancies/', VacancyListView.as_view(), name='vacancy-list'),
+    path('vacancies/<int:pk>/', VacancyDetailView.as_view(), name='vacancy-detail'),
+    path('vacancies/create/', VacancyCreateView.as_view(), name='vacancy-create'),
+    path('vacancies/update/<int:pk>/', VacancyUpdateView.as_view(), name='vacancy-update'),
+    path('vacancies/delete/<int:pk>/', VacancyDeleteView.as_view(), name='vacancy-delete'),
+
+    path('applications/', ApplicationListView.as_view(), name='application-list'),
+    path('applications/<int:pk>/', ApplicationDetailView.as_view(), name='application-detail'),
+    path('applications/create/', ApplicationCreateView.as_view(), name='application-create'),
+    path('applications/delete/<int:pk>/', ApplicationDeleteView.as_view(), name='application-delete'),
+
+    path('favorites/', FavoriteListView.as_view(), name='favorite-list'),
+    path('favorites/create/', FavoriteCreateView.as_view(), name='favorite-create'),
+
+    path('responses/', ResponseListView.as_view(), name='response-list'),
+    path('responses/create/', ResponseCreateView.as_view(), name='response-create'),
+
+    path('messages/', MessageListView.as_view(), name='message-list'),
+    path('messages/create/', MessageCreateView.as_view(), name='message-create'),
 ]
